b3c50c00c7e1ef27ce369c096fa9f9581dbbe693
Ritik Batra <ritikbatra@berkeley.edu>
2021-01-05 22:44:32 -0800

    test

--- src/screens/CreateTeamScreen.js
+++ src/screens/CreateTeamScreen.js
@@ -1,12 +1,67 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import TempDrawer from '../components/Drawer';
 import { Button, Container, Grid, TextField } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import * as dom from 'react-router-dom';
+// import firebase from "../constants/firebase";
 
+// const dbh = firebase.firestore();
+
+function checkIfExists(teamid) {
+
+    //     var docRef = dbh.collection(teamid);
+    //     console.log(teamid);
+
+    //     docRef.get().then(function (doc) {
+    //         if (doc.exists) {
+    //             return true;
+    //         } else {
+    //             // doc.data() will be undefined in this case
+    //             return false;
+    //         }
+    //     }).catch(function (error) {
+    //         console.log("Error getting document:", error);
+    //     });
+
+    // TODO: idk why its not working but check to make sure it doesn't already exist
+    return false;
+}
 
 export default function CreateTeamScreen() {
     const childRef = useRef();
+    const [teamId, setId] = useState("");
+    const [name, setName] = useState("");
+    const [length, setLength] = useState(7);
+    const [depth, setDepth] = useState(4);
+    const [invitations, setInvitations] = useState("");
+
+    /*
+        Create a unique ID (Should ensure that it doesn't already exist)
+    */
+    function makeUniqueID() {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        var charactersLength = characters.length;
+        while (result === '' || checkIfExists(result)) {
+            result = '';
+            for (var i = 0; i < 5; i++) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+        }
+        return result;
+    }
+
+    // Update the database
+    function newTeam() {
+        
+    }
+
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        // Update the document title using the browser API
+        setId(makeUniqueID());
+    }, []);
 
     return (
         <div>
@@ -20,76 +75,106 @@ export default function CreateTeamScreen() {
                     spacing={3}>
                     <Grid item xs={12}>
                         <h1>Formatio</h1>
-                    </Grid>
-                    <Grid item xs={12}>
                         <h2>Create Team</h2>
-                    </Grid>
-                    <Grid item xs={12}>
                         <FavoriteIcon style={{ fontSize: 45 }} />
                     </Grid>
 
-                    <Grid item xs={6}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Auto-Generated ID"
-                                name="email"
-                                autoComplete="email"
-                                variant="filled"
-                                autoFocus
-                            />
-                        </Grid>
-
-                        <Grid item xs={6}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                variant="filled"
-                                autoFocus
-                            />
-                        </Grid>
-
-                        <Grid item xs={6}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                variant="filled"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                            />
-                        </Grid>
-
+                    <Grid item xs={12}>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            id="standard-read-only-input"
+                            label="Team ID (Write this down)"
+                            name="teamid"
+                            variant="filled"
+                            value={teamId}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
                         <TextField
                             margin="normal"
                             required
                             fullWidth
-                            name="password"
+                            id="name"
+                            label="Team Name"
+                            name="name"
                             variant="filled"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
+                            value={name}
+                            onChange={(e) => (setName(e.target.value))}
+                            autoFocus
                         />
-                        <Button
-                            type="submit"
+                    </Grid>
+                    {/* TODO: Add a min/max to this of about 3 to 10 */}
+                    <Grid item xs={6}>
+                        <TextField
+                            margin="normal"
+                            required
                             fullWidth
-                            variant="contained"
-                            color="primary"
-                        >
-                            Sign In
-                             </Button>
+                            name="length"
+                            variant="filled"
+                            id="standard-number"
+                            label="Length"
+                            type="number"
+                            autoFocus
+                            value={length}
+                            onChange={(e) => (setLength(e.target.value))}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="depth"
+                            variant="filled"
+                            id="standard-number"
+                            label="Depth"
+                            type="number"
+                            autoFocus
+                            value={depth}
+                            onChange={(e) => (setDepth(e.target.value))}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            autoFocus
+                            variant="filled"
+                            fullWidth
+                            id="standard-multiline-static"
+                            label="Invitations (separate emails with comma)"
+                            multiline
+                            value={invitations}
+                            onChange={(e) => (setInvitations(e.target.value))}
+                            rows={3}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
 
+                        <dom.Link to="/create" style={{ textDecoration: 'none' }}>
+                            <Button
+                                onClick={newTeam}
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                            >
+                                Create Team
+                        </Button>
+                        </dom.Link>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <dom.Link to="/dashboard" variant="body2">
+                            {"Cancel"}
+                        </dom.Link>
+                    </Grid>
 
                 </Grid>
             </Container>
--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -2,6 +2,7 @@ import React, { useCallback, useRef } from 'react';
 import TempDrawer from '../components/Drawer';
 import { Button, Container, Grid } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import * as dom from 'react-router-dom';
 
 
 export default function DashboardScreen() {
@@ -20,13 +21,16 @@ export default function DashboardScreen() {
                     justify="center"
                     alignItems="center"
                     direction="column"
-                    style={{height: '80vh'}}
+                    style={{ height: '80vh' }}
                 >
                     <h1>Formatio</h1>
-                    <Button variant="contained" color="primary" style={{margin: '10px'}}>
-                        CREATE A NEW TEAM
+                    <dom.Link to="/create" style={{ textDecoration: 'none' }}>
+                        <Button variant="contained" color="primary" style={{ margin: '10px' }}>
+                            CREATE A NEW TEAM
                     </Button>
-                    <Button variant="contained" color="primary" style={{margin: '10px'}}>
+                    </dom.Link>
+
+                    <Button variant="contained" color="primary" style={{ margin: '10px' }}>
                         JOIN A TEAM
                     </Button>
 
